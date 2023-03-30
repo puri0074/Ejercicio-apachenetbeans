ac5e0a9c645ed9856550d4e2308fc88e553623ec
puri0074 <1472315@senati.pe>
2023-03-29 19:03:56 -0500

    hola mundo java

---
+++ holaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ejercicio.proyecto1;
+
+/**
+ *
+ * @author USER
+ */
+public class holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola mundo\n");
+        System.out.println("Hola mundo 2");
+        System.out.println("Hola mundo 3");
+        System.out.println("Hola mundo 4");
+    }
+    
+}
